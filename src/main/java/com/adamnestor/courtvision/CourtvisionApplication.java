7c30bde67d976d2f84ec20a8014906b7944f6d0a
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-11-21 07:12:21 -0500

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/adamnestor/courtvision/CourtvisionApplication.java
@@ -0,0 +1,13 @@
+package com.adamnestor.courtvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamnestor/courtvision/CourtvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamnestor.courtvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
