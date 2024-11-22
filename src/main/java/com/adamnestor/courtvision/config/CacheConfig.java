c6569186cf438bd93b3bec5bc1d2d0276c64e462
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-11-22 08:25:29 -0500

    Create CacheConfig.java

---
+++ src/main/java/com/adamnestor/courtvision/config/CacheConfig.java
@@ -0,0 +1,7 @@
+package com.adamnestor.courtvision.config;
+
+public interface CacheConfig {
+    String PLAYER_STATS_CACHE = "playerStats";
+    String HIT_RATES_CACHE = "hitRates";
+    String GAME_RESULTS_CACHE = "gameResults";
+}
