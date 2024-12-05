db187fc7e5e53c03fa2aef1f30cf8f7ad95fe0b1
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-12-05 15:27:14 -0500

    debugging 401 upon playerview click

--- src/main/java/com/adamnestor/courtvision/security/config/SecurityConfig.java
+++ src/main/java/com/adamnestor/courtvision/security/config/SecurityConfig.java
@@ -68,8 +68,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/api/dashboard/**").permitAll()
-                        .requestMatchers("/api/players/**").permitAll()
+                        .requestMatchers("/api/dashboard/stats").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
