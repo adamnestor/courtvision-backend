aea81b932f407f69e389c2b15ecec3f1f29def2d
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-12-09 16:01:30 -0500

    Update UserPicksRepository.java

--- src/main/java/com/adamnestor/courtvision/repository/UserPicksRepository.java
+++ src/main/java/com/adamnestor/courtvision/repository/UserPicksRepository.java
@@ -4,10 +4,17 @@ import com.adamnestor.courtvision.domain.UserPicks;
 import com.adamnestor.courtvision.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface UserPicksRepository extends JpaRepository<UserPicks, Long> {
     List<UserPicks> findByUser(Users user);
     List<UserPicks> findByUserOrderByCreatedAtDesc(Users user);
+    List<UserPicks> findByUserAndCreatedAtBetweenOrderByCreatedAtDesc(
+            Users user,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 }
