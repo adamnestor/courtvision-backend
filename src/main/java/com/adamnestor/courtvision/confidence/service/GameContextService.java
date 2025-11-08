1f9aea23aba6729cedacb31d27f2e6053c54b9c8
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-11-08 00:02:29 -0500

    delete confidence folder

--- src/main/java/com/adamnestor/courtvision/confidence/service/GameContextService.java
+++
@@ -1,8 +0,0 @@
-package com.adamnestor.courtvision.confidence.service;
-
-import com.adamnestor.courtvision.confidence.model.GameContext;
-import com.adamnestor.courtvision.domain.*;
-
-public interface GameContextService {
-    GameContext calculateGameContext(Players player, Games game, StatCategory category);
-}
--- src/main/java/com/adamnestor/courtvision/confidence/service/RestImpactService.java
+++
@@ -1,8 +0,0 @@
-package com.adamnestor.courtvision.confidence.service;
-
-import com.adamnestor.courtvision.confidence.model.RestImpact;
-import com.adamnestor.courtvision.domain.*;
-
-public interface RestImpactService {
-    RestImpact calculateRestImpact(Players player, Games currentGame);
-}
